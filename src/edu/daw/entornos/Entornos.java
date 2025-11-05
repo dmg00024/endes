997b340678da2965f621a2ce1823784628a1216f
UNAIPESCADOR\unaip <pescadorbenito.unai@jgvaldemora.org>
2025-11-05 13:57:38 +0100

    Commit entrega 5 primeras funciones

--- src/edu/daw/entornos/Entornos.java
+++ src/edu/daw/entornos/Entornos.java
@@ -3,28 +3,46 @@ public class Entornos {
     public static void main(String[] args) {
         int a=2;
         int b=6;
-        int c=b*2;
+        int c=2;
         int lado1=32;
         int lado2=20;
         int celsius=30;
         int far=32;
-
-
+        int sumaen=1+2+3+4+5+6+7+8+9+10;
         sumar(a, b);
         doble(a,c);
         area(lado1, lado2);
         grados(celsius, far);
-    }
-    public static void sumar(int a, int b){
-        System.out.println("El resultado de la suma es:"+(a+b));
-    }
-    public static void doble(int b, int c){
-        System.out.println("El resultado de la doble es:"+(c));
-    }
-    public static void area(int lado1, int lado2){
-        System.out.println("El resultado de la area es:"+(lado1*lado2));
-    }
-    public static void grados (int celsius, int far){
+        enteros(sumaen);
+
+
+        System.out.println("El resultado de la suma es:" + (a+b));
+        System.out.println("El resultado de la doble es:" + (a*c));
+        System.out.println("El resultado de la area es:" + (lado2*lado1));
         System.out.println("Los grados en fahrenheit:"+(celsius+far));
+    }
+    //Calcular la suma de dos números
+    public static int sumar(int a, int b){
+       return a+b ;
+    }
+
+    //Obtener el doble de un número
+    public static int doble(int b, int c){
+      return c ;
+    }
+
+    //Calcular el área de un cuadrado
+    public static int area(int lado1, int lado2){
+       return lado1*lado2;
+    }
+
+    //Convertir grados Celsius a Fahrenheit
+    public static int grados (int celsius, int far){
+        return celsius+far;
+    }
+
+    //Sumar los primeros 10 números naturales
+    public static int enteros(int sumaen){
+        return sumaen;
     }
 }
