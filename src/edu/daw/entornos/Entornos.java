fe505ca97fd1b4b61bfb7ad3ff797a549aa67706
Daniel Muñoz Gallardo <dmg00024@gmail.com>
2025-11-07 22:06:27 +0100

    DEVELOP NO SE TOCA

--- src/edu/daw/entornos/Entornos.java
+++ src/edu/daw/entornos/Entornos.java
@@ -22,24 +22,10 @@ import java.util.Scanner;
 
 public class Entornos {
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Introduce el primer número: ");
-        double num1 = sc.nextDouble();
-
-        System.out.print("Introduce el segundo número: ");
-        double num2 = sc.nextDouble();
-
-
-        double suma = num1 + num2;
-
-
-        System.out.println("La suma es: " + suma);
-
-        sc.close();
-
-
-
+        /**
+         * Develop no se toca bajo ningun concepto
+         * Todos disponemos de una branch personal para hacer pruebas
+         * y experimentar con el codigo.
+         */
     }
 }
