0ce7778bdd7d44ceb1181d5887e2a52bc6255eb3
LAPTOP-JUSKDSVK\Luis Miguel Lopez <kumonireal@gmail.com>
2025-12-02 18:05:46 +0100

    Añadidas Funciones de Suma de los 10 primeros naturales y unión de frases Añadidos tambien los system out de cada función EXTRAORDINARIA

--- src/Main.java
+++ src/Main.java
@@ -31,5 +31,27 @@ public static char ultimoCaracter(String texto) {
     /**obtenemos el ultimo caracter*/
     System.out.println("Último carácter: " + ultimoCaracter("Copilot"));
 }
+//6.Suma de los diez primeros numeros naturales
+public static int sumarPrimerosDiez() {
+    int suma = 0;
+    for (int i = 1; i <= 10; i++) {
+        suma += i;
+    }
+    return suma;
+
+}
+/**Llamamos la suma de los numeros naturales*/
+public static void main(String[] args) {
+    System.out.println("Suma primeros 10 naturales: " + sumarPrimerosDiez());
+}
+//7.Unir dos frases
+public static String unirFrases(String frase1, String frase2) {
+    return frase1 + " " + frase2;
+}
+/**unión de frases*/
+public static void main(String[] args) {
+    System.out.println("Frase unida: " + unirFrases("Hola Luismi,", "bienvenido a Java"));
+}
+
 
 
